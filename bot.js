4499f9c63bf2d05df764821166a1c55b605157c6
Xthekiller99X <46163620+Xthekiller99X@users.noreply.github.com>
2018-12-27 21:08:18 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -152,19 +152,14 @@ channel.guild.owner.send(`<@!${channelremover.id}>
   
 client.on('message', msg => {
 
-    if (msg.content == '!join') {
+    if (msg.content == '$join') {
         if (msg.member.voiceChannel) {
 
      if (msg.member.voiceChannel.joinable) {
          msg.member.voiceChannel.join().then(msg.react('white_check_mark'));
-     }
-    }
-}
-})
-client.on('ready', () => {
-    client.channels.get("527893643579883520").join(); 
+		 
+    client.channels.get("ايدي الروم").join(); 
     });
-	
 	client.on('guildMemberAdd', (member) => {
 member.addRole(member.guild.roles.find('name', 'souq'));  
 });
